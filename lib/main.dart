47526f7b2c3f0360da4b2173ef3daa986417b3e1
Rigoberto L. Perez <rigoberto.perez@allegion.com>
2021-11-02 14:41:17 -0400

    Make suggestions an infinite scrolling list

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter')
-        ),
-        body: const Center(
+      home: const Scaffold(
+        body: Center(
           child: RandomWords(),
         )
       )
@@ -45,9 +42,41 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerText = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (ctx, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerText,
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator')
+      ),
+      body: _buildSuggestions()
+    );
   }
 }
